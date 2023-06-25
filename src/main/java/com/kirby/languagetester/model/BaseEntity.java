9db8434df6b341b0520beae86abf553fed7a4727
iankirby <iankirby@localhost>
2023-06-25 16:25:19 +0100

    @GeneratedValue(strategy = GenerationType.IDENTITY)

--- src/main/java/com/kirby/languagetester/model/BaseEntity.java
+++ src/main/java/com/kirby/languagetester/model/BaseEntity.java
@@ -13,7 +13,7 @@ import javax.persistence.Version;
 public abstract class BaseEntity {
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	@Column(name = "lastUpdated")
 	private Timestamp lastUpdated;
