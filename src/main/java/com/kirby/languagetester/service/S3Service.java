f9ae4f0ef9e69179ac496ffa4f555eb23471b042
Ian.Kirby <iankirby1991@hotmail.com>
2023-07-14 20:01:20 +0100

    Increased Connection pool for S3 client

--- src/main/java/com/kirby/languagetester/service/S3Service.java
+++ src/main/java/com/kirby/languagetester/service/S3Service.java
@@ -5,6 +5,7 @@ import java.io.File;
 import org.springframework.stereotype.Service;
 
 import com.amazonaws.AmazonServiceException;
+import com.amazonaws.ClientConfiguration;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -16,35 +17,36 @@ import com.amazonaws.services.s3.model.S3Object;
 public class S3Service {
 
 	String bucket_name = "kirbylanguageapp";
-	final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_WEST_1).build();
-	
+	ClientConfiguration clientConfiguration = new ClientConfiguration().withMaxErrorRetry(10).withMaxConnections(350);
+	final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_WEST_1)
+			.withClientConfiguration(clientConfiguration).build();
 
 	public PutObjectResult uploadFile(String localFilePath, String awsUploadFilePath) {
-		PutObjectResult putObject=null;
+		PutObjectResult putObject = null;
 		try {
-			 putObject = s3.putObject(bucket_name, awsUploadFilePath, new File(localFilePath));
+			putObject = s3.putObject(bucket_name, awsUploadFilePath, new File(localFilePath));
 			putObject.getMetadata();
 		} catch (AmazonServiceException e) {
 			System.err.println(e.getErrorMessage());
 			System.exit(1);
 		}
 		return putObject;
-		
+
 	}
 
 	public boolean getAWSFileURL(String path) {
 
-		 S3Object object=null;
-		 try {
-			 object=s3.getObject(bucket_name, path);
-		 }catch(AmazonS3Exception e) {
-			  if (e.getStatusCode() == 404) {
-	                System.out.println("Object does not exist in S3.");
-	            } else {
-	                System.out.println("An error occurred while checking object existence: " + e.getMessage());
-	            }
-		 }
-		 return object!=null;
+		S3Object object = null;
+		try {
+			object = s3.getObject(bucket_name, path);
+		} catch (AmazonS3Exception e) {
+			if (e.getStatusCode() == 404) {
+				System.out.println("Object does not exist in S3.");
+			} else {
+				System.out.println("An error occurred while checking object existence: " + e.getMessage());
+			}
+		}
+		return object != null;
 	}
 
 }
