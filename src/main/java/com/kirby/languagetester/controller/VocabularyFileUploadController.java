b3a9aaa0a06093f6652434af7f8adbc0353fc988
Ian.Kirby <iankirby1991@hotmail.com>
2023-07-15 21:16:42 +0100

    Added Auth header to Vocab upload

--- src/main/java/com/kirby/languagetester/controller/VocabularyFileUploadController.java
+++ src/main/java/com/kirby/languagetester/controller/VocabularyFileUploadController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +31,7 @@ public class VocabularyFileUploadController {
 	}
 
 	@PostMapping("/upload")
-	public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) throws Exception {
+	public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file,@RequestHeader(value = "Authorization") String token) throws Exception {
 		try {
 			byte[] bytes = file.getBytes();
 			String directoryPath = "/tmp/uploads/";
