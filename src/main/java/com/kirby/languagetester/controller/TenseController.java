e87c0e825839b888bebd3546d1cba1b3c247d32e
Ian.Kirby <iankirby1991@hotmail.com>
2023-11-22 21:48:19 +0000

    test 4

--- src/main/java/com/kirby/languagetester/controller/TenseController.java
+++ src/main/java/com/kirby/languagetester/controller/TenseController.java
@@ -19,7 +19,7 @@ import com.kirby.languagetester.repository.TenseRepository;
 @RestController
 @RequestMapping("api/tenses")
 public class TenseController {
-	//test3
+	//test4
 
 	private TenseRepository tenseRepository;
 
